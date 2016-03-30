3f1041aa2724b424daea7b6637f12b4ab48448ad
Sotirios-Efstathios Maneas <smaneas@gmail.com>
2016-03-29 23:46:21 -0400

    Implemented a new recursive cat command. Minor changes in the Makefile and .gitignore files.

--- file.c
+++ file.c
@@ -4,80 +4,151 @@
 #include "tx.h"
 #include "fslice.h"
 
+int cmd_cat(struct super_block *sb, struct context *c) {
+	char *buf;
+	int inode_nr;
+	struct inode *in;
+	int ret = 0;
+	int sz;
+	int i;
 
-int
-cmd_cat(struct super_block *sb, struct context *c)
-{
-        char *buf;
-        int inode_nr;
-        struct inode *in;
-        int ret = 0;
-        int sz;
-        int i;
+	if (c->nargs < 2) {
+		return -EINVAL;
+	}
 
-        if (c->nargs < 2) {
-                return -EINVAL;
-        }    
-        for (i = 1; ret == 0 && i < c->nargs; i++ )
-        {
-                inode_nr = testfs_dir_name_to_inode_nr(c->cur_dir, c->cmd[i]);
-                if (inode_nr < 0)
-                        return inode_nr;
-                in = testfs_get_inode(sb, inode_nr);
-                if (testfs_inode_get_type(in) == I_DIR) {
-                        ret = -EISDIR;
-                        goto out;
-                }
-                sz = testfs_inode_get_size(in);
-                if (sz > 0) {
-                        buf = malloc(sz + 1);
-                        if (!buf) {
-                                ret = -ENOMEM;
-                                goto out;
-                        }
-                        testfs_read_data(in, 0, buf, sz);
-                        buf[sz] = 0;
-                        printf("%s\n", buf);
-                        free(buf);
-                }
-out:
-                testfs_put_inode(in);                
-        }
+	printf("Invoking cmd_cat with: %d, %s, %p\n", c->nargs, c->cmd[1], c->cur_dir);
 
-        return ret;
+	for (i = 1; ret == 0 && i < c->nargs; i++) {
+		inode_nr = testfs_dir_name_to_inode_nr(c->cur_dir, c->cmd[i]);
+		if (inode_nr < 0)
+			return inode_nr;
+		in = testfs_get_inode(sb, inode_nr);
+		if (testfs_inode_get_type(in) == I_DIR) {
+			ret = -EISDIR;
+			goto out;
+		}
+		sz = testfs_inode_get_size(in);
+		if (sz > 0) {
+			buf = malloc(sz + 1);
+			if (!buf) {
+				ret = -ENOMEM;
+				goto out;
+			}
+			testfs_read_data(in, 0, buf, sz);
+			buf[sz] = 0;
+			printf("%s\n", buf);
+			free(buf);
+		}
+		out: testfs_put_inode(in);
+	}
+
+	return ret;
 }
 
-int
-cmd_write(struct super_block *sb, struct context *c)
-{
-        int inode_nr;
-        struct inode *in;
-        int size;
-        int ret = 0;
-        char * filename = c->cmd[1];
-        char * content = c->cmd[2];
+int cmd_catr(struct super_block *sb, struct context *c) {
+	char *cdir = ".";
+	int inode_nr;
+	struct inode *in, *tmp_inode;
+	int offset = 0;
+	struct dirent *d;
+	int ret = 0;
+	int sz;
+	char *buf;
 
-        if (c->nargs != 3) {
-                return -EINVAL;
-        }
-        inode_nr = testfs_dir_name_to_inode_nr(c->cur_dir, filename);
-        if (inode_nr < 0)
-                return inode_nr;
-        in = testfs_get_inode(sb, inode_nr);
-        if (testfs_inode_get_type(in) == I_DIR) {
-                ret = -EISDIR;
-                goto out;
-        }
-        size = strlen(content);
-        testfs_tx_start(sb, TX_WRITE);
-        ret = testfs_write_data(in, 0, content, size);
-        if (ret >= 0) {
-                testfs_truncate_data(in, size);
-        }
-        testfs_sync_inode(in);
-        testfs_tx_commit(sb, TX_WRITE);
-out:
-        testfs_put_inode(in);
-        return ret;
+	printf("Invoking cmd_catr with: %d, %s, %p\n", c->nargs, c->cmd[1], c->cur_dir);
+
+	if (c->nargs > 2) {
+		return -EINVAL;
+	}
+
+	if (c->nargs == 2) {
+		cdir = c->cmd[1];
+	}
+	assert(c->cur_dir);
+
+	/* Store the current directory. */
+	tmp_inode = c->cur_dir;
+
+	/* Get the inode number that corresponds to the provided
+	 * directory name. If no directory name is specified, search
+	 * for the current directory. */
+	inode_nr = testfs_dir_name_to_inode_nr(c->cur_dir, cdir);
+	if (inode_nr < 0)
+		return inode_nr;
+
+	/* Get the corresponding inode object. */
+	in = testfs_get_inode(sb, inode_nr);
+
+	for (; (d = testfs_next_dirent(in, &offset)); free(d)) {
+		struct inode *cin;
+
+		if (d->d_inode_nr < 0)
+			continue;
+
+		if((strcmp(D_NAME(d), ".") != 0) && (strcmp(D_NAME(d), "..") != 0)) {
+			cin = testfs_get_inode(testfs_inode_get_sb(in), d->d_inode_nr);
+
+			if (testfs_inode_get_type(cin) == I_DIR) {
+				c->cur_dir = cin;
+				ret = cmd_catr(sb, c);
+				if(ret < 0) {
+					testfs_put_inode(cin);
+					goto out;
+				}
+				c->cur_dir = tmp_inode;
+			}
+			else {
+				printf("%s:\n", D_NAME(d));
+				sz = testfs_inode_get_size(cin);
+				if (sz > 0) {
+					buf = malloc(sz + 1);
+					if (!buf) {
+						testfs_put_inode(cin);
+						ret = -ENOMEM;
+						goto out;
+					}
+					testfs_read_data(cin, 0, buf, sz);
+					buf[sz] = 0;
+					printf("%s\n", buf);
+					free(buf);
+				}
+			}
+			testfs_put_inode(cin);
+		}
+	}
+	out: testfs_put_inode(in);
+
+	return 0;
+}
+
+int cmd_write(struct super_block *sb, struct context *c) {
+	int inode_nr;
+	struct inode *in;
+	int size;
+	int ret = 0;
+	char * filename = c->cmd[1];
+	char * content = c->cmd[2];
+
+	if (c->nargs != 3) {
+		return -EINVAL;
+	}
+	inode_nr = testfs_dir_name_to_inode_nr(c->cur_dir, filename);
+	if (inode_nr < 0)
+		return inode_nr;
+	in = testfs_get_inode(sb, inode_nr);
+	if (testfs_inode_get_type(in) == I_DIR) {
+		ret = -EISDIR;
+		goto out;
+	}
+	size = strlen(content);
+	testfs_tx_start(sb, TX_WRITE);
+	ret = testfs_write_data(in, 0, content, size);
+	if (ret >= 0) {
+		testfs_truncate_data(in, size);
+	}
+	testfs_sync_inode(in);
+	testfs_tx_commit(sb, TX_WRITE);
+	out: testfs_put_inode(in);
+	return ret;
 }
 
