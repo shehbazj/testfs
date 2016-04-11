1d803e410cebd1eb2aa90d4aae3bd45f2f8beaea
Shehbaz Jaffer <shehbazjaffer007@gmail.com>
2016-04-11 14:29:19 -0400

    Added Workload generator
    Creates a file system based on the percentage of filesystem that
    needs to be filled up.
    
    1st directory - 2 level directory dir1/dir1_1/file1
    2nd directory - 1 level directory dir2/dir2_2
    3rd directory - 1 level directory dir3/file2
    
    all other directories and files are created inside root directory
    
    Added README

---
+++ workload/createFileSystem.sh
@@ -0,0 +1,27 @@
+	
+	usage="\n\t./createFileSystem.sh <percentage_full> <distribution>
+	\n percentage_full 25 50 75
+	\n distribution 0 - all files created are equal in size
+	\n		1 - file sizes have binary distribution
+	\n\tgenerates workload for file system to occupy percent_full inodes and data blocks\n"
+	
+	if [[ $# -ne 2 ]]; 
+	then
+		echo -e "$usage";
+		exit 1
+	fi
+
+	INODE=128
+	DBLOCK=512
+	
+	percent=$1
+	distribution=$2
+	numInode=$(($((percent * $INODE))/100))
+	numDataBlock=$(($((percent * $DBLOCK))/100))
+
+	numDir=$((numInode / 2))
+	numFile=$((numInode / 2))
+
+#	echo "percent $percent, numInode = $numInode, numDataBlock = $numDataBlock"	
+	./makeFileAndDir.sh $numDir $numFile
+	./generateData.sh $numDir $numFile $numDataBlock $distribution
---
+++ workload/makeFileAndDir.sh
@@ -0,0 +1,48 @@
+	
+	usage="\n\t./makeFileAndDir.sh <numDir> <numFile>\n
+	\n\tgenerates touch and mkdir commands to create files and directories"
+	
+	if [[ $# -ne 2 ]]; 
+	then
+		echo -e "$usage";
+		exit 1
+	fi
+	
+	numDir=$1
+	numFile=$2
+	
+	if [[ $numDir -le 3 || $numFile -le 3 ]];
+	then
+	# create all files and directories in the root directory itself
+	
+	# create directories
+		for i in seq `1 $numDir`; do
+			mkdir dir$i
+		done		
+	# create files
+		for i in seq `1 $numFile`; do
+			touch file$i
+		done		
+	else
+	# create two level directory with 1 file i.e. dir1/dir1_2/file1
+
+	echo -e "mkdir dir1\ncd dir1\nmkdir dir1_2\ncd dir1_2\ntouch file1\ncd"
+	
+	# create 1 level directory with 1 directory i.e. dir2/dir2_2
+	echo -e "mkdir dir2\ncd dir2\nmkdir dir2_2\ncd"
+	
+	# create 1 level directory with 1 file i.e. dir3/file2
+	echo -e "mkdir dir3\ncd dir3\ntouch file2\ncd"
+	
+	# create all other files and directories in / directory	
+	for i in `seq 3 $numFile`; do
+		echo "touch file$i"
+	done
+
+	for i in `seq 4 $numDir`; do
+		echo "mkdir dir$i"
+	done
+	
+	fi
+
+
