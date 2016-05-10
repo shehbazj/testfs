63f26d9a8a3503812c135351108a6a9527fbf36f
Shehbaz Jaffer <shehbazjaffer007@gmail.com>
2016-05-10 01:26:10 -0400

    Clustering algorithm, adding second loop

--- visualize/post_processing/cluster.py
+++ visualize/post_processing/cluster.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import sys
 import shutil
 import os
@@ -27,37 +29,45 @@ if __name__ == "__main__":
 allFiles=sys.argv[1:len(sys.argv)]
 #print allFiles
 
-f1=getNextFile()
-nextFile+=1
 #print "f1 returned",f1
-while f1:
-	f2=getNextFile()
-	#print "f2 returned",f2
-	if f2 is None:
-		break
-	if commonCksum(f1,f2) == True:
-		#print f1,f2,"Match"
-		f=f1+'.'+f2
-		#print "name of f",f," after merging ",f1,f2
-		mergeFile=open(f, "w")
-		fileOne=open(f1,"r")
-		fileTwo=open(f2,"r")
-		mergeFile.write(fileOne.read())
-		mergeFile.write(fileTwo.read())
-		mergeFile.close()
-		fileOne.close()
-		fileTwo.close()
-		os.remove(f1)
-		os.remove(f2)
-		allFiles.remove(f1)
-		allFiles.remove(f2)
-		allFiles.insert(0,f)		
-		#print "all Files = ",allFiles
-		f1=f
+while True:
+	f1=getNextFile()
+	nextFile+=1
+	while f1:
+		f2=getNextFile()
+		#print "f2 returned",f2
+		if f2 is None:
+			break
+		if commonCksum(f1,f2) == True:
+			#print f1,f2,"Match"
+			f=f1+'.'+f2
+			#print "name of f",f," after merging ",f1,f2
+			mergeFile=open(f, "w")
+			fileOne=open(f1,"r")
+			fileTwo=open(f2,"r")
+			mergeFile.write(fileOne.read())
+			mergeFile.write(fileTwo.read())
+			mergeFile.close()
+			fileOne.close()
+			fileTwo.close()
+			os.remove(f1)
+			os.remove(f2)
+			allFiles.remove(f1)
+			allFiles.remove(f2)
+			allFiles.insert(0,f)		
+			#print "all Files = ",allFiles
+			f1=f
+		else:
+			nextFile+=1
+			#print f1,f2,"No Match"
+			#f1=f2
+	if f1 is not None:
+		pos = allFiles.index(f1)	
+		if pos < 0 or pos > len(allFiles) -1:
+			break
+		else:
+			nextFile = pos+1	
 	else:
-		nextFile+=1
-		#print f1,f2,"No Match"
-		f1=f2
-
+		break
 #print allFiles
 #print commonCksum(f1,f2)
